639c98fe59d3f2d35e940162ca2dbf908980f688
Julian <julian_kellner@outlook.com>
2023-12-10 18:16:53 +0100

    Add activities

--- server.js
+++ server.js
@@ -5,17 +5,20 @@ const app = express()
 app.use(express.static('public'))
 app.use(express.json())
 
-const dataBaseFilePath = 'db/users.json'
+const userDataBaseFilePath = 'db/users.json'
+const activitiesDatabaseFilePath = 'db/activities.json'
+
+let activities = loadActivitiesFromDatabase()
 
 app.post('/login', async(req, res) => {
     let jsonData = req.body
     const username = jsonData['username']
     const password = jsonData['password']
 
-    fs.readFile(dataBaseFilePath, 'utf8', (err, fileData) => {
+    fs.readFile(userDataBaseFilePath, 'utf8', (err, fileData) => {
         if(err){
             console.error('Error reading file:', err)
-            res.status(500).send('Internal Server Error')
+            res.sendStatus(500)
             return;
         }
 
@@ -24,11 +27,11 @@ app.post('/login', async(req, res) => {
             existingData = JSON.parse(fileData)
         }catch(parseError){
             console.error('Error parsing existing JSON:', parseError)
-            res.status(500).send('Internal Server Error')
+            res.sendStatus(500)
             return
         }
 
-        const dataExists = checkDataExists(existingData, jsonData)
+        const dataExists = checkUserDataExists(existingData, jsonData)
 
         if(dataExists){
             const correctPassword = checkPassword(username, password, existingData)
@@ -45,17 +48,12 @@ app.post('/login', async(req, res) => {
         
         const updatedData = {...existingData, ...jsonData}
 
-        const jsonString = JSON.stringify(updatedData, null, 2)
-
-        fs.writeFile(dataBaseFilePath, jsonString, 'utf8', (writeError) => {
-            if(writeError){
-                console.error('Error writing JSON to file:', writeError)
-                res.status(500).send('Internal Server Error')
-            }else{
-                res.status(200)
-                res.redirect('/voting.html')
-            }
-        })
+        if(writeJsonToFile(userDataBaseFilePath, updatedData)){
+            res.sendStatus(200)
+            res.redirect('/voting.html')
+        }else{
+            res.sendStatus(500)
+        }
     })
 })
 
@@ -64,10 +62,10 @@ app.post('/auth-voting', async (req, res) => {
     const username = jsonData['username']
     const password = jsonData['password']
 
-    fs.readFile(dataBaseFilePath, 'utf8', (err, fileData) => {
+    fs.readFile(userDataBaseFilePath, 'utf8', (err, fileData) => {
         if(err){
             console.error('Error reading file:', err)
-            res.status(500).send('Internal Server Error')
+            res.sendStatus(500)
             return;
         }
 
@@ -76,11 +74,11 @@ app.post('/auth-voting', async (req, res) => {
             existingData = JSON.parse(fileData)
         }catch(parseError){
             console.error('Error parsing existing JSON:', parseError)
-            res.status(500).send('Internal Server Error')
+            res.sendStatus(500)
             return
         }
 
-        const dataExists = checkDataExists(existingData, jsonData)
+        const dataExists = checkUserDataExists(existingData, jsonData)
 
         if(dataExists){
             const correctPassword = checkPassword(username, password, existingData)
@@ -94,12 +92,60 @@ app.post('/auth-voting', async (req, res) => {
     })
 })
 
-function checkDataExists(existingData, newData){
+app.post('/submit-new-activity', async (req, res) => {
+    const jsonData = req.body
+
+    const newActivity = {
+        description: jsonData['description'],
+        author: jsonData['author'],
+        votingScore: 0
+    }
+
+    activities[Object.getOwnPropertyNames(activities).length] = newActivity
+
+    if(!writeJsonToFile(activitiesDatabaseFilePath, activities)){
+        res.sendStatus(200)
+    }else{
+        res.sendStatus(500)
+    }
+})
+
+app.get('/get-activities', async (req, res) => {
+    res.status(200).json(activities)
+})
+
+function checkUserDataExists(existingData, newData){
     return existingData[newData['username']] !== undefined
 }
 
 function checkPassword(username, password, fileData){
     return username !== undefined && password !== undefined && fileData[username] === password
+}
+
+function writeJsonToFile(dataBaseFilePath, jsonData){
+    fs.writeFile(dataBaseFilePath, JSON.stringify(jsonData, null, 2), 'utf8', (writeError) => {
+        if(writeError){
+            console.error('Error writing JSON to file:', writeError)
+            return false
+        }else{
+            return true
+        }
+    })
+}
+
+function loadActivitiesFromDatabase(){
+    fs.readFile(activitiesDatabaseFilePath, (err, fileData) => {
+        if(err){
+            console.error('Error reading file:', err)
+            return;
+        }
+    
+        try{
+            activities = JSON.parse(fileData)
+        }catch(parseError){
+            console.error('Error parsing existing JSON:', parseError)
+        }
+    })
 }
 
 app.listen(6969)
